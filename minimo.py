7659a505a998e541db88a0b32b8ca4e6eb3c5b89
francorzo <35819505+francorzo@users.noreply.github.com>
2018-07-11 18:19:41 -0500

    Add files via upload

---
+++ minimo.py
@@ -0,0 +1,28 @@
+import math
+import sys 
+def biseccion(x1,x2):
+   x3=1
+   while evalf(x3) > 0.0002 or evalf(x3) < -.0002:
+      x3=(x1+x2)/2
+      if evalf(x3) * evalf(x1) > 0:
+         x1=x3
+      else:
+         x2=x3
+   return x3
+
+def evalf(x):
+   f = 2 * x
+   return f
+
+print ("Ingrese el valor del primer punto")
+x1=input()
+print ("Ingrese el valor del segundo punto")
+x2=input()
+
+if evalf(x1)*evalf(x2)>0:
+   print("No se puede determinar un minimo")
+else: 
+   raiz=biseccion(x1,x2) 
+   print "El minimo es = ", raiz
+
+
